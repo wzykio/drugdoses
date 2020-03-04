9b52101fa8609802b204715eb11228eb4ee2f0dd
bartek <bartlomiejszczypior@gmail.com>
2020-03-04 08:53:42 +0100

    init header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './Header.module.css'
+
+const Header = () =>{
+
+    return(
+        <div className={style.header}>Header</div>
+    )
+}
+export default Header;
