5dac8e65685083e78d71f3d4fab8321b61c366a2
bartek <bartlomiejszczypior@gmail.com>
2020-03-04 20:08:24 +0100

    update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,16 @@
 import React from "react";
 import style from "./Header.module.css";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Header = () => {
   return (
     <div className={style.header}>
+      <div className={style.navicon}>
+        <GiHamburgerMenu  />
+      </div>
+
       <div className={style.title}>
-        <p>DAWKI LEKÓW</p>
+        <p>Dawki leków u dzieci</p>
       </div>
     </div>
   );
