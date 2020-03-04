d274cede66bdf01c139fbd40db8d27c3a99d5979
bartek <bartlomiejszczypior@gmail.com>
2020-03-04 09:39:50 +0100

    adding styles to the header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from 'react';
-
-import './App.css';
-import Header from './components/Header/Header';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-     <Header/>
+      <Header />
     </div>
   );
 }
-
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import style from './Header.module.css'
+import React from "react";
+import style from "./Header.module.css";
 
-const Header = () =>{
-
-    return(
-        <div className={style.header}>Header</div>
-    )
-}
+const Header = () => {
+  return (
+    <div className={style.header}>
+      <div className={style.title}>
+        <p>DAWKI LEKÓW</p>
+      </div>
+    </div>
+  );
+};
 export default Header;
