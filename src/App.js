211910737d6892993c4a2fdddab56d9ffab83ca0
bartek <bartlomiejszczypior@gmail.com>
2020-03-05 15:13:50 +0100

    add and styling block

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Warning from "./components/Warning/Warning";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Warning />
     </div>
   );
 }
