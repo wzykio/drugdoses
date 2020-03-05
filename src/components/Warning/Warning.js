ab269ed1a266416a38d37b02c3490d8cbd0ac8e8
bartek <bartlomiejszczypior@gmail.com>
2020-03-05 15:26:46 +0100

    after correction

--- src/components/Warning/Warning.js
+++ src/components/Warning/Warning.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import style from './Warning.module.css'
+import React from "react";
+import style from "./Warning.module.css";
 
-const Warning = () =>{
-return(
-<div className={style.warningBlock}>
-    <h1 className={style.warnHeader}>UWAGA! Używaj tylko jako POMOC !!!</h1>
-    <p>Każdy pacjent to indywidualna sytuacja. Farmakoterapie i jej dawki powinny być weryfikowane pod względem każdego pacjenta. </p>
-
-</div>
-
-)
-
-}
+const Warning = () => {
+  return (
+    <div className={style.warningBlock}>
+      <h1 className={style.warnHeader}>UWAGA! Używaj tylko jako POMOC !!!</h1>
+      <p>
+        Każdy pacjent to indywidualna sytuacja. Farmakoterapie i jej dawki
+        powinny być weryfikowane pod względem każdego pacjenta.
+      </p>
+    </div>
+  );
+};
 
 export default Warning;
