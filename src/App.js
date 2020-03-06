3d9c7cb102caabd05dd29d2d35747c70c150b9c4
bartek <bartlomiejszczypior@gmail.com>
2020-03-06 18:53:10 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Button from "./components/Button/Button"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Button />
     </div>
   );
 }
