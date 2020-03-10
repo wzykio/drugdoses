331e1e8d788ea396e16a6922a8035c1b8e3f8ce1
bartek <bartlomiejszczypior@gmail.com>
2020-03-10 13:24:52 +0100

    poprawka

--- src/components/Weight/Weight.js
+++ src/components/Weight/Weight.js
@@ -2,31 +2,31 @@ import React, { useState } from "react";
 import style from "./Weight.module.css";
 
 const Weight = () => {
-  const [weight, setWeight] = useState(null);
+  let [weight, setWeight] = useState(1);
   const minusOne = () => {
-    if (weight > 1) {
-      setWeight(weight - 1);
+    if (weight >= 5) {
+      const numMinusOne = Number(weight);
+      setWeight(numMinusOne - 1);
     } else {
       alert("Waga nie może być ujemna!");
     }
   };
   const minusFive = () => {
     if (weight >= 5) {
-      setWeight(weight - 5);
+      const numMinusFive = Number(weight);
+      setWeight(numMinusFive - 5);
     } else {
       alert("Waga nie może być ujemna!");
     }
   };
 
   const plusOne = () => {
-    
-      setWeight(weight + 1);
-   
+    const numPlusOne = Number(weight);
+    setWeight(numPlusOne + 1);
   };
   const plusFive = () => {
-  
-      setWeight(weight + 5);
-   
+    const numPlusFive = Number(weight);
+    setWeight(numPlusFive + 5);
   };
   const chandleChangeInput = e => {
     setWeight(e.target.value);
@@ -47,8 +47,12 @@ const Weight = () => {
           value={weight}
           onChange={chandleChangeInput}
         ></input>
-        <button className={style.weightButton} onClick={plusOne} >+1kg</button>
-        <button className={style.weightButton} onClick={plusFive}>+5kg</button>
+        <button className={style.weightButton} onClick={plusOne}>
+          +1kg
+        </button>
+        <button className={style.weightButton} onClick={plusFive}>
+          +5kg
+        </button>
       </div>
     </div>
   );
