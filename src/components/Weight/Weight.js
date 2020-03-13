3fb1fde862800d5eb18cca14117bec40b2240467
KamilW <kamil.wezyk@gmail.com>
2020-03-13 13:22:29 +0100

    Alternative version of Weight.js functionality

--- src/components/Weight/Weight.js
+++ src/components/Weight/Weight.js
@@ -2,33 +2,16 @@ import React, { useState, useEffect } from "react";
 import style from "./Weight.module.css";
 
 const Weight = ({ get }) => {
-  let [weight, setWeight] = useState(1);
+  let [weight, setWeight] = useState(1)
   useEffect(get.bind(null, weight));
-  const minusOne = () => {
-    if (weight >= 1) {
-      const numMinusOne = Number(weight);
-      setWeight(numMinusOne - 1);
-    } else {
+  
+  useEffect(() => {
+    if(weight < 1){
+      setWeight(1)
       alert("Waga nie może być ujemna!");
-    }
-  };
-  const minusFive = () => {
-    if (weight >= 5) {
-      const numMinusFive = Number(weight);
-      setWeight(numMinusFive - 5);
-    } else {
-      alert("Waga nie może być ujemna!");
-    }
-  };
-
-  const plusOne = () => {
-    const numPlusOne = Number(weight);
-    setWeight(numPlusOne + 1);
-  };
-  const plusFive = () => {
-    const numPlusFive = Number(weight);
-    setWeight(numPlusFive + 5);
-  };
+    };
+  },[weight]);
+  
   const chandleChangeInput = e => {
     setWeight(e.target.value);
   };
@@ -37,10 +20,10 @@ const Weight = ({ get }) => {
     <div className={style.weightWrapper}>
       <h1>Waga dziecka : </h1>
       <div className={style.buttons}>
-        <button className={style.weightButton} onClick={minusOne}>
+        <button className={style.weightButton} onClick={()=>setWeight(Number(weight) - 1)}>
           -1kg
         </button>
-        <button className={style.weightButton} onClick={minusFive}>
+        <button className={style.weightButton} onClick={()=>setWeight(Number(weight) - 5)}>
           -5kg
         </button>
         <input
@@ -49,10 +32,10 @@ const Weight = ({ get }) => {
           value={weight}
           onChange={chandleChangeInput}
         ></input>
-        <button className={style.weightButton} onClick={plusOne}>
+        <button className={style.weightButton} onClick={()=>setWeight(Number(weight) + 1)}>
           +1kg
         </button>
-        <button className={style.weightButton} onClick={plusFive}>
+        <button className={style.weightButton} onClick={()=>setWeight(Number(weight) + 5)}>
           +5kg
         </button>
         
