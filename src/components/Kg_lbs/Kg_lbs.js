f8124858e17946b27c0aec8ef014a831194ae5e4
KamilW <kamil.wezyk@gmail.com>
2020-03-14 21:34:08 +0100

    KgLbs + zmiana vw see description
    zmieniłem wszystkie vw (viewport width) we wszystkich plikach css gdzie było 100 na 98
    Niestaty na przeglądarkach telefonowych był tzw efekt wobble'a poziomego (strona przesuwała się minimalnie w poziomie, a nie powinna) ta zmiana, może nie elegancka ale wyeliminowała problem

--- src/components/Kg_lbs/Kg_lbs.js
+++ src/components/Kg_lbs/Kg_lbs.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import style from "./Kg_lbs.module.css";
+import classNames from 'classnames';
 import { FaCheck } from "react-icons/fa";
 const Kg_lbs = () => {
   const [activelbs, setActivelbs] = useState(false);
@@ -13,19 +14,27 @@ const Kg_lbs = () => {
     setActivelbs(false);
   };
   return (
-    <div className={style.buttonWrapper}>
-      <label className={activeKg ? style.button : style.buttonUnactive}>
-        <FaCheck
-          className={activeKg ? style.checkedIcon : style.checkedIconUnactive}
-        />
+    <div className={style.wrapper}>
+      <label 
+       style={{borderRadius: '4px 0 0 4px'}}
+       className={classNames({
+          [style.button] : true,
+          [style.inactive] : !activeKg
+        })}
+      >
+        <FaCheck />
         <p>kg</p>
         <input type="radio" checked={activeKg} onChange={selectKg} />
       </label>
 
-      <label className={activelbs ? style.button : style.buttonUnactive}>
-        <FaCheck
-          className={activelbs ? style.checkedIcon : style.checkedIconUnactive}
-        />
+      <label
+       style={{borderRadius: '0 4px 4px 0'}}
+       className={classNames({
+          [style.button] : true,
+          [style.inactive] : !activelbs
+        })}
+      >
+        <FaCheck />
         <p>lbs</p>
         <input type="radio" checked={activelbs} onChange={selectlbs} />
       </label>
