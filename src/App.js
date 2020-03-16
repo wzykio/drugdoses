f8504613b61a0d4ea86c3f154fd452c1803e22ff
KamilW <kamil.wezyk@gmail.com>
2020-03-16 15:27:33 +0100

    Auto stash before rebase of "master"

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
-import Button from "./components/Button/Button";
-import Weight from "./components/Weight/Weight";
-import Card from "./components/Cards/Card";
-import Kglbs from "./components/Kg_lbs/Kg_lbs";
-import Menu from "./components/Menu/Menu";
+import ExampleComponent from "./components/ExampleComponent/ExampleComponent";
 
 function App() {
-  const [finalweight, setFinalweight] = useState(null);
-  const addWeight = finalweight => {
-    setFinalweight(finalweight);
-  };
 
   return (
     <div className="App">
-      <Header />
-      <Menu />
-      <Button />
-      <Weight get={addWeight} />
-      <Kglbs />
-      <Card weight={finalweight} />
-      
+      <ExampleComponent></ExampleComponent>
     </div>
   );
 }
---
+++ src/components/ExampleComponent/ExampleComponent.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react"
+import style from "./ExampleComponent.module.css"
+
+const ExampleComponent = () => {
+    return <>
+        <div className={style.element}>Hello!</div>
+    </>
+}
+
+export default ExampleComponent
