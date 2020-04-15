ceef153277b9ce58a850e15a607ebee11e6b15ff
ireshka <ireshka@users.noreply.github.com>
2020-04-15 19:00:29 +0200

    Handling state change in language switcher component

--- src/components/LanguageSwitcher/LanguageSwitcher.js
+++ src/components/LanguageSwitcher/LanguageSwitcher.js
@@ -1,36 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './LanguageSwitcher.module.css';
 
-const LanguageSwitcher = () => (
-  <>
-    <fieldset className={style.wrapper}>
-      <h3 className={style.heading}>Wybór języka</h3>
-      <div className={style.radio}>
-        <label className={style.label}>
-          <input
-            className={style.radio__input}
-            type='radio'
-            name='language'
-            value='polish'
-          />
-          <span className={style.radio__button}></span>
-          <span className={style.radio__label}>Polski</span>
-        </label>
-      </div>
-      <div className={style.radio}>
-        <label className={style.label}>
-          <input
-            className={style.radio__input}
-            type='radio'
-            name='language'
-            value='english'
-          />
-          <span className={style.radio__button}></span>
-          <span className={style.radio__label}>Angielski</span>
-        </label>
-      </div>
-    </fieldset>
-  </>
-);
+const LanguageSwitcher = () => {
+  const [activeLanguage, setActiveLanguage] = useState('polish');
+
+  const setLanguage = (language) => {
+    setActiveLanguage(language);
+    console.log('Active language is: '+language);
+  }
+
+  return (
+    <>
+      <fieldset className={style.wrapper}>
+        <h3 className={style.heading}>Wybór języka</h3>
+        <div className={style.radio}>
+          <label className={style.label}>
+            <input
+              className={style.radio__input}
+              type='radio'
+              name='language'
+              value='polish'
+              onChange={() => setLanguage('polish')}
+              checked={activeLanguage ==='polish'}
+            />
+            <span className={style.radio__button}></span>
+            <span className={style.radio__label}>Polski</span>
+          </label>
+        </div>
+        <div className={style.radio}>
+          <label className={style.label}>
+            <input
+              className={style.radio__input}
+              type='radio'
+              name='language'
+              value='english'
+              onChange={() => setLanguage('english')}
+              checked={activeLanguage ==='english'}
+            />
+            <span className={style.radio__button}></span>
+            <span className={style.radio__label}>Angielski</span>
+          </label>
+        </div>
+      </fieldset>
+    </>
+  );
+};
 
 export default LanguageSwitcher;
