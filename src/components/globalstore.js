bf1dc445d99b4102682ce7e8425c63c1f130a423
bartek <bartlomiejszczypior@gmail.com>
2020-04-15 12:34:34 +0200

    jezyk w aplikacji taki jak w przegladarce

--- src/components/globalstore.js
+++ src/components/globalstore.js
@@ -1,16 +1,23 @@
-import React from "react";
-
+import React from 'react'
+let userLang = navigator.language || navigator.userLanguage; 
 const lang = {
-  pl: {
-    header: {
-      top: "Dawki leków u dzieci",
-    },
-  },
-  en: {
-    header: {
-      top: "Drug doses...",
-    },
-  },
-};
-const LangContext = React.createContext(lang.pl);
+        pl:{
+             header:{
+                 top: "Dawki leków u dzieci"
+             }
+        },
+        en:{
+         header:{
+             top: "Drug doses..."
+         }
+     }
+     
+     
+       }
+const LangContext = React.createContext(userLang === "pl-PL" ? lang.pl : lang.en);
 export default LangContext;
+
+
+     
+
+
