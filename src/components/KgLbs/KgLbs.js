0e6dda8a2356afaa0f1c615c284536b0e76b7ffb
bartek <bartlomiejszczypior@gmail.com>
2020-04-16 11:02:49 +0200

    aktualizacja 16.04.2020

--- src/components/KgLbs/KgLbs.js
+++ src/components/KgLbs/KgLbs.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import style from "./KgLbs.module.css";
 import classNames from "classnames";
 import { FaCheck } from "react-icons/fa";
@@ -6,17 +6,17 @@ import { FaCheck } from "react-icons/fa";
 const KgLbs = (/*{ getUni }*/) => {
   const [activelbs, setActivelbs] = useState(false);
   const [activeKg, setKg] = useState(true);
-  const [active, setActive] = useState(true);
- // useEffect(getUni.bind(true, active));
+ 
+  // useEffect(getUni.bind(true, active));
   const selectlbs = () => {
     setActivelbs(true);
     setKg(false);
-    setActive(false);
+   
   };
   const selectKg = () => {
     setKg(true);
     setActivelbs(false);
-    setActive(true);
+   
   };
   return (
     <div className={style.wrapper}>
@@ -24,7 +24,7 @@ const KgLbs = (/*{ getUni }*/) => {
         style={{ borderRadius: "4px 0 0 4px" }}
         className={classNames({
           [style.button]: true,
-          [style.inactive]: !activeKg
+          [style.inactive]: !activeKg,
         })}
       >
         <FaCheck />
@@ -36,7 +36,7 @@ const KgLbs = (/*{ getUni }*/) => {
         style={{ borderRadius: "0 4px 4px 0" }}
         className={classNames({
           [style.button]: true,
-          [style.inactive]: !activelbs
+          [style.inactive]: !activelbs,
         })}
       >
         <FaCheck />
