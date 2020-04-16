0d7e30eb94d09412bcd682294f9c5cdd3d731c27
ireshka <ireshka@users.noreply.github.com>
2020-04-16 16:25:58 +0200

    Refactor Language Switcher component

--- src/components/LanguageSwitcher/LanguageSwitcher.js
+++ src/components/LanguageSwitcher/LanguageSwitcher.js
@@ -2,44 +2,48 @@ import React, { useState } from 'react';
 import style from './LanguageSwitcher.module.css';
 
 const LanguageSwitcher = () => {
-  const [activeLanguage, setActiveLanguage] = useState('polish');
+  const [activeLanguage, setActiveLanguage] = useState('pl');
 
   const setLanguage = (language) => {
     setActiveLanguage(language);
-  }
+  };
+
+  let availableLanguages = [
+    {
+      name: 'Polski',
+      shortcut: 'pl',
+    },
+    {
+      name: 'Angielski',
+      shortcut: 'en',
+    },
+  ];
 
   return (
     <>
       <fieldset className={style.wrapper}>
         <h3 className={style.heading}>Wybór języka</h3>
-        <div className={style.radio}>
-          <label className={style.label}>
-            <input
-              className={style.radio__input}
-              type='radio'
-              name='language'
-              value='polish'
-              onChange={() => setLanguage('polish')}
-              checked={activeLanguage ==='polish'}
-            />
-            <span className={style.radio__button}></span>
-            <span className={style.radio__label}>Polski</span>
-          </label>
-        </div>
-        <div className={style.radio}>
-          <label className={style.label}>
-            <input
-              className={style.radio__input}
-              type='radio'
-              name='language'
-              value='english'
-              onChange={() => setLanguage('english')}
-              checked={activeLanguage ==='english'}
-            />
-            <span className={style.radio__button}></span>
-            <span className={style.radio__label}>Angielski</span>
-          </label>
-        </div>
+        {availableLanguages.map((lang) => {
+          const { name } = lang;
+          const { shortcut } = lang;
+
+          return (
+            <div className={style.radio} key={shortcut}>
+              <label className={style.label}>
+                <input
+                  className={style.radio__input}
+                  type='radio'
+                  name='language'
+                  value={shortcut}
+                  onChange={() => setLanguage(shortcut)}
+                  checked={activeLanguage === shortcut}
+                />
+                <span className={style.radio__button}></span>
+                <span className={style.radio__label}>{name}</span>
+              </label>
+            </div>
+          );
+        })}
       </fieldset>
     </>
   );
