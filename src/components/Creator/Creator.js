5b078d1d8fab0f638d01864b4639d0cc2992eed8
ireshka <ireshka@users.noreply.github.com>
2020-04-21 14:03:41 +0200

    Add about section with dummy data

---
+++ src/components/Creator/Creator.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import style from './Creator.module.css';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+
+const Creator = ({ creator }) => {
+  const { name, avatar, linkedin, github, description } = creator;
+  return (
+    <section className={style.section}>
+      <div className={style.wrapper}>
+        <img src={avatar} alt={name} className={style.avatar} />
+        <div className={style.info}>
+          <h4 className={`${style.info__element} ${style.info__name}`}>{name}</h4>
+          <a href={linkedin} className={`${style.info__element} ${style.link}`}>
+            <FaLinkedin className={style.icon}/>Linkedin Profile
+          </a>
+          <a href={github}  className={`${style.info__element} ${style.link}`}>
+            <FaGithub className={style.icon}/>Github Profile
+          </a>
+        </div>
+      </div>
+      <p className={style.description}>{description}</p>
+    </section>
+  );
+};
+
+export default Creator;
