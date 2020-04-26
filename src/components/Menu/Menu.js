7e2aabb7a9f96c631c1c05519fdf807880dedae5
Marcin <marcin.luczakk@gmail.com>
2020-04-26 22:51:12 +0200

    add hidden menu funcionality

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,67 +1,76 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import style from "./Menu.module.css";
 import { FaCog, FaDiaspora } from "react-icons/fa"; //ustawienia
 import { MdAnnouncement } from "react-icons/md"; //nota prawna
 import { GoInfo } from "react-icons/go"; //o nas
 import { StoreContext } from "../../states/Store";
-import { ClickAwayListener } from "@material-ui/core";
 
 const Menu = ({ openMenuOption }) => {
   const [state, dispatch] = useContext(StoreContext);
 
-  const handleClickAway = () => {
-    dispatch({ type: "OPEN_MENU" });
-  };
+  const clickAway = useRef(null);
+
+  useEffect(() => {
+    function handleClickAway(e) {
+      if (clickAway.current && !clickAway.current.contains(e.target)) {
+        dispatch({ type: "OPEN_MENU" });
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickAway);
+    return () => {
+      document.removeEventListener("mousedown", handleClickAway);
+    };
+  }, [dispatch]);
+
   return (
-    <ClickAwayListener onClickAway={handleClickAway}>
-      <div className={style.menuBlock}>
-        <ul>
-          <li
-            className={style.listItem}
-            onClick={() => {
-              dispatch({ type: "OPEN_SETTINGS" });
-              dispatch({ type: "OPEN_MENU" });
-              dispatch({ type: "OPEN_MENU_OPTION" });
-            }}
-          >
-            <FaCog className={style.icon} />
-            Ustawienia
-          </li>
-          <li
-            className={style.listItem}
-            onClick={() => {
-              dispatch({ type: "OPEN_LEGAL" });
-              dispatch({ type: "OPEN_MENU" });
-              dispatch({ type: "OPEN_MENU_OPTION" });
-            }}
-          >
-            <MdAnnouncement className={style.icon} />
-            Nota prawna
-          </li>
-          <li
-            className={style.listItem}
-            onClick={() => {
-              dispatch({ type: "OPEN_ABOUT" });
-              dispatch({ type: "OPEN_MENU" });
-              dispatch({ type: "OPEN_MENU_OPTION" });
-            }}
-          >
-            <GoInfo className={style.icon} />O nas
-          </li>
-          <li
-            className={style.listItem}
-            onClick={() => {
-              dispatch({ type: "OPEN_LEGEND" });
-              dispatch({ type: "OPEN_MENU" });
-              dispatch({ type: "OPEN_MENU_OPTION" });
-            }}
-          >
-            <FaDiaspora className={style.icon} />
-            Legenda
-          </li>
-        </ul>
-      </div>
-    </ClickAwayListener>
+    <div ref={clickAway} className={style.menuBlock}>
+      <ul>
+        <li
+          className={style.listItem}
+          onClick={() => {
+            dispatch({ type: "OPEN_SETTINGS" });
+            dispatch({ type: "OPEN_MENU" });
+            dispatch({ type: "OPEN_MENU_OPTION" });
+          }}
+        >
+          <FaCog className={style.icon} />
+          Ustawienia
+        </li>
+        <li
+          className={style.listItem}
+          onClick={() => {
+            dispatch({ type: "OPEN_LEGAL" });
+            dispatch({ type: "OPEN_MENU" });
+            dispatch({ type: "OPEN_MENU_OPTION" });
+          }}
+        >
+          <MdAnnouncement className={style.icon} />
+          Nota prawna
+        </li>
+        <li
+          className={style.listItem}
+          onClick={() => {
+            dispatch({ type: "OPEN_ABOUT" });
+            dispatch({ type: "OPEN_MENU" });
+            dispatch({ type: "OPEN_MENU_OPTION" });
+          }}
+        >
+          <GoInfo className={style.icon} />O nas
+        </li>
+        <li
+          className={style.listItem}
+          onClick={() => {
+            dispatch({ type: "OPEN_LEGEND" });
+            dispatch({ type: "OPEN_MENU" });
+            dispatch({ type: "OPEN_MENU_OPTION" });
+          }}
+        >
+          <FaDiaspora className={style.icon} />
+          Legenda
+        </li>
+      </ul>
+    </div>
   );
 };
 
