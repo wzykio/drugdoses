5f55ee7218f3fca0428b24cb24abeef709c543f3
ireshka <ireshka@users.noreply.github.com>
2020-05-14 18:34:41 +0200

    Add choose medical case button translate

--- src/states/localization/index.js
+++ src/states/localization/index.js
@@ -13,6 +13,10 @@ export const lang = {
       consent1: 'Rozumiem',
       consent2: 'Biorę odpowiedzialność za dobór dawek'
     },
+    medicalCaseChooseButton: {
+      cardiac: 'Zatrzymanie krążenia',
+      other: 'Leki w innych przypadkach'
+    },
     menu: {
       settings: 'Ustawienia',
       legalNote: 'Nota prawna',
@@ -57,6 +61,10 @@ export const lang = {
       consent1: 'Got it',
       consent2: 'I take responsibility for a dose selection'
     },
+    medicalCaseChooseButton: {
+      cardiac: 'Cardiac arrest',
+      other: 'Drugs in other cases'
+    },
     menu: {
       settings: 'Settings',
       legalNote: 'Legal Note',
@@ -67,7 +75,8 @@ export const lang = {
       about: {
         heading: 'About',
         subheadingCompany: 'About Company',
-        subheadingTeam: 'Our team'
+        subheadingTeam: 'Our team',
+        contentCompany: `Be superior. Kitten is playing with dead mouse. Fall asleep upside-down mark territory, yet instead of drinking water from the cat bowl, make sure to steal water from the toilet. Cry louder at reflection. Enslave the hooman. Hide head under blanket so no one can see give me some of your food give me some of your food give me some of your food meh, i don't want it pretend you want to go out but then don't chase red laser dot, for bury the poop bury it deep. Soft kitty warm kitty little ball of furr. Meow in empty rooms experiences short bursts of poo-phoria after going to the loo.`
       },
       legal: {
         heading: 'Legal note'
