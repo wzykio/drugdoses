4d4ad15fc3653d1acefca97ab9e9d90a191a0f15
ireshka <ireshka@users.noreply.github.com>
2020-05-16 21:10:53 +0200

    Round to 1 decimal place

--- src/components/Otherdrugs/Otherdrugs.js
+++ src/components/Otherdrugs/Otherdrugs.js
@@ -9,7 +9,7 @@ const Otherdrugs = () => {
   const { drugsOther } = lang[langu];
   let weight = state.weight * units;
 
-  const round = (number) => Math.round(number * 100) / 100;
+  // const round = (number) => Math.round(number * 100) / 100;
 
   return (
     <div className={viewEny ? style.otherdrugsWrapper : style.unvisible}>
@@ -17,9 +17,9 @@ const Otherdrugs = () => {
         <h2>{drugsOther.adrenaline.drugName}</h2>
         <p>{drugsOther.adrenaline.anaphylaxis}</p>
         <p>
-          {drugsOther.adrenaline.asthma}: {round(weight / 100)} mg -{' '}
+          {drugsOther.adrenaline.asthma}: {(weight / 100).toFixed(1)} mg -{' '}
           {drugsOther.adrenaline.asthmaPreparation}.{' '}
-          {drugsOther.administer} {round(weight / 10)} ml s.c.{' '}
+          {drugsOther.administer} {(weight / 10).toFixed(1)} ml s.c.{' '}
         </p>
       </div>
 
@@ -27,12 +27,12 @@ const Otherdrugs = () => {
         <h2>{drugsOther.adenosine.drugName}</h2>
         <p>{drugsOther.adenosine.preparation}</p>
         <p>
-          {drugsOther.adenosine.dose1} {round(weight / 10)} mg i.v.
-          - {round(weight / 10)} ml{' '}
+          {drugsOther.adenosine.dose1} {(weight / 10).toFixed(1)} mg i.v.
+          - {(weight / 10).toFixed(1)} ml{' '}
         </p>
         <p>
-          {drugsOther.adenosine.dose2} {(weight / 10) * 2} mg i.v. -{' '}
-          {(weight / 10) * 2} ml
+          {drugsOther.adenosine.dose2} {((weight / 10) * 2).toFixed(1)} mg i.v. -{' '}
+          {((weight / 10) * 2).toFixed(1)} ml
         </p>
       </div>
 
@@ -42,19 +42,19 @@ const Otherdrugs = () => {
         <p>{drugsOther.dexamethasone.asthma}</p>
         <p>
           {drugsOther.dexamethasone.laryngitis}{' '}
-          {(0.15 * weight).toFixed(2)} mg i.v.
+          {(0.15 * weight).toFixed(1)} mg i.v.
         </p>
       </div>
 
       <div className={style.otherdrugsItemDiazepam}>
         <h2>{drugsOther.diazepam.drugName}</h2>
         <p>
-          {drugsOther.dose} {(0.25 * weight).toFixed(2)} mg
+          {drugsOther.dose} {(0.25 * weight).toFixed(1)} mg
           i.v./i.o.{' '}
         </p>
         <p>
           {drugsOther.diazepam.administration}{' '}
-          {(0.05 * weight).toFixed(2)} ml
+          {(0.05 * weight).toFixed(1)} ml
         </p>
         <div className={style.alert}>
           {drugsOther.diazepam.alert}
@@ -64,7 +64,7 @@ const Otherdrugs = () => {
       <div className={style.otherdrugsItemFlumazenil}>
         <h2>{drugsOther.flumazenil.drugName}</h2>
         <p>
-          {drugsOther.dose} {10 * weight} i.v./i.o µg.{' '}
+          {drugsOther.dose} {(10 * weight).toFixed(1)} i.v./i.o µg.{' '}
           {drugsOther.flumazenil.doseDetails}
         </p>
         <p>{drugsOther.flumazenil.administration}</p>
@@ -73,21 +73,21 @@ const Otherdrugs = () => {
       <div className={style.otherdrugsItemHydrocortison}>
         <h2>{drugsOther.hydrocortisone.drugName}</h2>
         <p>
-          {drugsOther.dose} {weight * 5} mg
+          {drugsOther.dose} {(weight * 5).toFixed(1)} mg
         </p>
         <p>{drugsOther.hydrocortisone.preparation}</p>
         <p>
-          {drugsOther.administer} {weight} ml i.v./i.o.
+          {drugsOther.administer} {weight.toFixed(1)} ml i.v./i.o.
         </p>
       </div>
 
       <div className={style.otherdrugsItemMagnez}>
         <h2>{drugsOther.magnesium.drugName}</h2>
         <p>
-          {drugsOther.dose} {weight * 40} mg
+          {drugsOther.dose} {(weight * 40).toFixed(1)} mg
         </p>
         <p>
-          {drugsOther.magnesium.administration} {(weight * 2) / 10}{' '}
+          {drugsOther.magnesium.administration} {((weight * 2) / 10).toFixed(1)}{' '}
           ml i.v/i.o.
         </p>
       </div>
@@ -95,18 +95,18 @@ const Otherdrugs = () => {
       <div className={style.otherdrugsItemMorfina}>
         <h2>{drugsOther.morphine.drugName}</h2>
         <p>
-          {drugsOther.dose} {round(weight / 10)} mg
+          {drugsOther.dose} {(weight / 10).toFixed(1)} mg
         </p>
         <p>{drugsOther.morphine.preparation}</p>
         <p>
-          {drugsOther.administer} {round(weight / 10)} ml i.v/i.o.
+          {drugsOther.administer} {(weight / 10).toFixed(1)} ml i.v/i.o.
         </p>
       </div>
 
       <div className={style.otherdrugsItemParacetamol}>
         <h2>{drugsOther.paracetamol.drugName}</h2>
         <p>
-          {drugsOther.dose} {weight * 10} mg p.r.
+          {drugsOther.dose} {(weight * 10).toFixed(1)} mg p.r.
         </p>
       </div>
     </div>
